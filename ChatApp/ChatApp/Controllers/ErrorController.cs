1c5ae1c380dc78d2c89e17aced23af51fd097d26
punio7 <punio7@gmail.com>
2017-10-08 12:32:44 +0200

    Nowe pliki

---
+++ ChatApp/ChatApp/Controllers/ErrorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebCrawler.ChatApp.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return RedirectToAction("GenericError", new HandleErrorInfo(new HttpException(403, "Dont allow access the error pages"), "ErrorController", "Index"));
+        }
+
+        public ViewResult GenericError(HandleErrorInfo exception)
+        {
+            return View("Error", exception);
+        }
+
+        public ViewResult NotFound(HandleErrorInfo exception)
+        {
+            ViewBag.Title = "Page Not Found";
+            return View("Error", exception);
+        }
+    }
+}
